3ae21d636b2370ee9f04f164f963235fb23b45b4
harshtekriwal <harshtekriwal12@gmail.com>
2020-05-22 16:41:37 +0530

    Update auth.dart

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -33,7 +33,7 @@ class Auth with ChangeNotifier {
       String email, String password, String urlSegment) async {
          
     final url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$WebApiKey.WEBAPIKEY';
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=${WebApiKey.WEBAPIKEY}';
     try {
       final response = await http.post(url,
           body: json.encode({
